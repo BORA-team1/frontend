38bf0f3a38a8337e2f71e6e0d386b4fc6228cf1c
mod_siw <pvq_71@ewhain.net>
2023-08-08 19:44:04 +0900

    style : 화면 위치 및 폰트 수정

--- src/pages/DetailEditorPage.js
+++ src/pages/DetailEditorPage.js
@@ -11,25 +11,27 @@ import X from "../images/X.svg";
 
 // props로 받아올 posts 구조 분해 할당
 const DetailEditorPage = () => {
-    const navigate = useNavigate();
-    const path = window.location.pathname;
-    const navigatorM = () => {
-        navigate("/mypage"); //이거 함수 불러올 수 있으면 안 써도 되지 않나?
-    };
-    return (
-        <Container>
-            <Scroll>
-                <>
-                    <Box>
-                        {/* 듣는 아티클 부분 */}
-                        <Del src={X} onClick={navigatorM} />
-                        <Title>팔로우한 에디터</Title>
-                    </Box>
-                    <div style={{ margin: "50px" }}>에디터 페이지입니다</div>
-                </>
-            </Scroll>
-        </Container>
-    );
+  const navigate = useNavigate();
+  const path = window.location.pathname;
+  const navigatorM = () => {
+    navigate("/mypage"); //이거 함수 불러올 수 있으면 안 써도 되지 않나?
+  };
+  return (
+    <Container>
+      <Scroll>
+        <>
+          <Box>
+            {/* 듣는 아티클 부분 */}
+            <Del src={X} onClick={navigatorM} />
+            <Title>팔로우한 에디터</Title>
+          </Box>
+          <div style={{ margin: "50px", fontFamily: "Pretendard-Regular" }}>
+            에디터 페이지입니다
+          </div>
+        </>
+      </Scroll>
+    </Container>
+  );
 };
 
 export default DetailEditorPage;
@@ -37,57 +39,57 @@ export default DetailEditorPage;
 //전체 styled
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    position: relative;
+  display: flex;
+  flex-direction: column;
+  position: relative;
 
-    max-width: 390px;
-    max-height: 844px;
-    margin: 0px auto;
+  max-width: 390px;
+  max-height: 844px;
+  margin: 0px auto;
 
-    background-color: #161524;
-    color: #fff;
+  background-color: #161524;
+  color: #fff;
 `;
 
 const Scroll = styled.div`
-    overflow-y: scroll;
-    height: 730px;
+  overflow-y: scroll;
+  height: 730px;
 
-    &::-webkit-scrollbar {
-        display: none;
-    }
+  &::-webkit-scrollbar {
+    display: none;
+  }
 
-    position: relative;
-    z-index: 0;
+  position: relative;
+  z-index: 0;
 `;
 
 const Box = styled.div`
-    display: flex;
-    flex-direction: row;
-    height: 80px;
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  flex-direction: row;
+  height: 80px;
+  align-items: center;
+  justify-content: center;
 
-    position: relative;
+  position: relative;
 
-    border-bottom: 1px solid #353646;
+  border-bottom: 1px solid #353646;
 `;
 
 const Title = styled.div`
-    margin: 20px;
-    color: #fff;
-    font-family: "Pretendard";
-    font-size: 15px;
-    font-style: normal;
-    font-weight: 600;
-    line-height: 100%; /* 15px */
-    letter-spacing: -0.3px;
+  margin: 20px;
+  color: #fff;
+  font-family: "Pretendard-Regular";
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 100%; /* 15px */
+  letter-spacing: -0.3px;
 `;
 
 const Del = styled.img`
-    position: absolute;
-    left: 20px;
-    width: 18px;
-    height: 18px;
-    margin: 20px;
+  position: absolute;
+  left: 20px;
+  width: 18px;
+  height: 18px;
+  margin: 20px;
 `;
