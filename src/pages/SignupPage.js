d2996e26ef81082b712c082f111248eb41ffa32e
mod_siw <pvq_71@ewhain.net>
2023-08-14 22:04:10 +0900

    회원가입 필요 요건 로직 추가

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import axios from "axios";
 
 //context
@@ -15,6 +15,7 @@ const SignupPage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [nickname, setNickname] = useState("");
+  const [interest, setInterest] = useState("");
   const [profile, setProfile] = useState("");
   // const [gender, setGender] = useState("");
   const [age, setAge] = useState("");
@@ -26,32 +27,52 @@ const SignupPage = () => {
 
   const handleSignup = async (e) => {
     e.preventDefault();
-    await axios
-      .post(`${BASE_URL}account/signup/`, {
-        username: username,
-        password: password,
-        nickname: nickname,
-        profile: profile,
-        // gender: gender,
-        age: age,
-      })
-      .then((response) => {
-        //회원가입 성공했을 때
-        setRender(render + 1);
+
+    if (birthYear) {
+      const calculatedAgeGroup = calculateAgeGroup(Number(birthYear));
+      setAge(calculatedAgeGroup);
+
+      try {
+        const response = await axios.post(`${BASE_URL}account/signup/`, {
+          username: username,
+          password: password,
+          nickname: nickname,
+          interest: interest,
+          profile: profile,
+          age: calculatedAgeGroup,
+        });
+
+        // 회원가입 성공 시 처리
+        setAge("");
         navigate(`/login`);
         console.log(response);
-      })
-      .catch((error) => console.log(error));
+      } catch (error) {
+        // 오류 처리
+        console.error(error);
+      }
+    }
+  };
+
+  //아이디 규격 확인
+  const [usernameValid, setUsernameValid] = useState(true);
+
+  const handleUsernameChange = (e) => {
+    const value = e.target.value;
+    const alphanumericPattern = /^[a-zA-Z0-9]{6,}$/;
+
+    if (alphanumericPattern.test(value)) {
+      setUsernameValid(true);
+    } else {
+      setUsernameValid(false);
+    }
+
+    setUsername(value);
   };
 
   //비밀번호 일치 확인
   const [passwordConfirm, setPasswordConfirm] = useState("");
   const [passwordMatch, setPasswordMatch] = useState(false);
-  const [passwordValid, setPasswordValid] = useState(true);
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
+  const [passwordValid, setPasswordValid] = useState(false);
 
   const handlePasswordConfirmChange = (e) => {
     setPasswordConfirm(e.target.value);
@@ -66,7 +87,10 @@ const SignupPage = () => {
   }, [password, passwordConfirm]);
 
   useEffect(() => {
-    //비밀번호 규격 맞는지 확인하는 로직
+    setPasswordValid(isPasswordValid(password));
+  }, [password]);
+
+  const isPasswordValid = (password) => {
     const hasLowerCase = /[a-z]/.test(password);
     const hasUpperCase = /[A-Z]/.test(password);
     const hasDigit = /\d/.test(password);
@@ -74,16 +98,81 @@ const SignupPage = () => {
       password
     );
 
-    if (
+    return (
       password.length >= 10 &&
       [hasLowerCase, hasUpperCase, hasDigit, hasSpecialChar].filter(Boolean)
         .length >= 2
-    ) {
-      setPasswordValid(true);
-    } else {
-      setPasswordValid(false);
+    );
+  };
+
+  //연령층 계산
+  const [birthYear, setBirthYear] = useState(""); // 출생 연도 입력값
+
+  const calculateAgeGroup = (birthYear) => {
+    const today = new Date();
+    const currentYear = today.getFullYear();
+    const age = currentYear - birthYear + 1;
+
+    let ageGroup = "";
+    if (age >= 10 && age < 20) {
+      ageGroup = "1";
+    } else if (age >= 20 && age < 30) {
+      ageGroup = "2";
+    } else if (age >= 30 && age < 40) {
+      ageGroup = "3";
+    } else if (age >= 40 && age < 50) {
+      ageGroup = "4";
+    } else if (age >= 50) {
+      ageGroup = "5";
     }
-  }, [password]);
+
+    return ageGroup;
+  };
+
+  // 생년월일 입력 처리
+  const handleBirthYearChange = (e) => {
+    const value = e.target.value;
+    // 숫자만 남기도록 처리
+    const numericValue = value.replace(/\D/g, "");
+
+    // 4글자 이상 입력되지 않도록 제한
+    if (numericValue.length <= 4) {
+      setBirthYear(numericValue);
+    }
+  };
+
+  //이미지 미리보기
+  const [selectedImage, setSelectedImage] = useState(null); // 선택한 이미지 파일
+
+  const handleImageChange = (e) => {
+    const imageFile = e.target.files[0];
+    setSelectedImage(URL.createObjectURL(imageFile));
+    // 이미지 선택 시 setProfile 호출
+    setProfile(URL.createObjectURL(imageFile));
+  };
+
+  //버튼 활성화
+  const [requiredFieldsValid, setRequiredFieldsValid] = useState(false);
+  useEffect(() => {
+    // Calculate requiredFieldsValid based on all required fields
+    const isRequiredFieldsValid =
+      usernameValid &&
+      passwordValid &&
+      passwordMatch &&
+      birthYear.length === 4 &&
+      nickname.length > 0 &&
+      profile.length > 0;
+
+    setRequiredFieldsValid(isRequiredFieldsValid);
+  }, [
+    usernameValid,
+    passwordValid,
+    passwordMatch,
+    birthYear,
+    nickname,
+    interest,
+    profile,
+  ]);
 
   return (
     <Wrapper>
@@ -105,11 +194,17 @@ const SignupPage = () => {
             type="text"
             placeholder="예 : bora1234"
             value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            onChange={handleUsernameChange}
           />
           <Check>중복확인</Check>
-          <Condition>영문과 숫자를 조합하여 6자 이상</Condition>
-          <ConditionGood>사용 가능한 아이디입니다.</ConditionGood>
+          {usernameValid ? (
+            <Condition>영문과 숫자를 조합하여 6자 이상</Condition>
+          ) : (
+            <Condition style={{ color: "#FF5E2B" }}>
+              아이디가 형식에 맞지 않습니다. 다시 입력해주십시오.
+            </Condition>
+          )}
+          {/* <ConditionGood>사용 가능한 아이디입니다.</ConditionGood> */}
         </ID>
         <div style={{ marginBottom: "30px" }}>
           <Guide>
@@ -160,36 +255,45 @@ const SignupPage = () => {
           <Guide>
             생년월일 <span>*</span>
           </Guide>
-          <Input
+          <InputY
             type="text"
-            placeholder="나이"
-            value={age}
-            onChange={(e) => setAge(e.target.value)}
+            placeholder="YYYY"
+            value={birthYear}
+            onChange={handleBirthYearChange}
           />
+          <InputM type="text" placeholder="MM" />
+          <InputD type="text" placeholder="DD" />
+          <Condition>왼쪽부터 년 (YYYY), 월 (MM), 일 (DD) 순 입력</Condition>
         </div>
         <div style={{ marginBottom: "50px" }}>
           <Guide>관심사</Guide>
-          <Input
+          <InputI
             type="text"
             placeholder="예 : #라이프, #건강, #테크, #문화, #경제, #환경"
-            value={profile}
-            onChange={(e) => setProfile(e.target.value)}
+            value={interest}
+            onChange={(e) => setInterest(e.target.value)}
           />
+          <Condition>관심사 및 선호하는 주제를 '#'를 이용하여 입력</Condition>
         </div>
         <div>
           <Guide>프로필 이미지</Guide>
-          <Condition>
+          <ConditionP>
             창밖을보라에서 사용할 프로필 사진을 선택해 주세요.
-          </Condition>
-          <InputImg
-            type="file"
-            accept="image/*"
-            placeholder="프로필"
-            value={profile}
-            onChange={(e) => setProfile(e.target.value)}
-          />
+          </ConditionP>
+          <InputImgContainer>
+            <InputImg
+              type="file"
+              accept="image/*"
+              onChange={handleImageChange}
+            />
+            {selectedImage && (
+              <PreviewImage src={selectedImage} alt="Profile Preview" />
+            )}
+          </InputImgContainer>
         </div>
-        <Btn onClick={handleSignup}>회원가입</Btn>
+        <Btn onClick={handleSignup} disabled={!requiredFieldsValid}>
+          회원가입
+        </Btn>
       </Container>
     </Wrapper>
   );
@@ -226,6 +330,26 @@ const Container = styled.div`
   }
 `;
 
+const InputGuide = styled.input`
+  border-radius: 5px;
+  background: var(--card-color, #2b2c3f);
+  box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.25);
+  border: none;
+  outline: none;
+
+  font-family: "Pretendard-Regular";
+  font-size: 13px;
+  font-style: normal;
+  color: #fff;
+
+  padding-left: 10px;
+  margin: 10px 0px;
+
+  ::placeholder {
+    color: rgba(255, 255, 255, 0.6);
+  }
+`;
+
 //상단바 부분
 const Box = styled.div`
   display: flex;
@@ -255,11 +379,6 @@ const Del = styled.img`
 `;
 
 //내용 부분
-// const InputWrapper = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   margin-top: 30px;
-// `;
 
 const Guide = styled.div`
   font-family: "Pretendard-Regular";
@@ -272,32 +391,15 @@ const Guide = styled.div`
     color: var(--sub-purple, #a397ff);
   }
 `;
-const Input = styled.input`
-  font-family: "Pretendard-Regular";
-  font-size: 13px;
-  font-style: normal;
-  color: #fff;
-
+const Input = styled(InputGuide)`
   width: 340px;
   height: 46px;
-  padding-left: 10px;
-  margin: 10px 0px;
-
-  border-radius: 5px;
-  background: var(--card-color, #2b2c3f);
-  box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.25);
-  border: none;
-  outline: none;
-
-  ::placeholder {
-    color: rgba(255, 255, 255, 0.6);
-  }
 `;
 
 const Condition = styled.div`
   font-family: "Pretendard-Regular";
   color: rgba(255, 255, 255, 0.6);
-  font-size: 12px;
+  font-size: 10px;
   font-style: normal;
   font-weight: 400;
 `;
@@ -317,26 +419,10 @@ const ID = styled.div`
   margin-bottom: 60px;
 `;
 
-const InputID = styled.input`
-  font-family: "Pretendard-Regular";
-  font-style: normal;
-  font-size: 13px;
-  color: #fff;
-
+const InputID = styled(InputGuide)`
   width: 241px;
   height: 46px;
-  padding-left: 10px;
-  margin: 10px 0px;
-
-  border-radius: 5px;
-  background: var(--card-color, #2b2c3f);
-  box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.25);
-  border: none;
-  outline: none;
-
-  ::placeholder {
-    color: rgba(255, 255, 255, 0.6);
-  }
+  margin-right: 10px;
 `;
 
 const Check = styled.button`
@@ -354,13 +440,33 @@ const Check = styled.button`
   font-weight: 600;
 `;
 
-//이미지
-const InputImg = styled.input`
-  font-family: "Pretendard-Regular";
-  font-style: normal;
+//생년월일
 
-  width: 131px;
-  height: 131px;
+const InputY = styled(InputGuide)`
+  width: 81px;
+  height: 46px;
+`;
+
+const InputM = styled(InputGuide)`
+  width: 71px;
+  height: 46px;
+
+  margin: 10px;
+`;
+
+const InputD = styled(InputGuide)`
+  width: 71px;
+  height: 46px;
+`;
+
+//이미지
+
+const InputImgContainer = styled.div`
+  width: 141px;
+  height: 141px; //임의로 수정
+  position: relative;
+  display: inline-block;
+
   border-radius: 5px;
   border-radius: 5.001px;
   border: 0.5px dashed #fff;
@@ -368,6 +474,36 @@ const InputImg = styled.input`
   outline: none;
 
   margin-bottom: 60px;
+  margin-top: 25px;
+`;
+
+const InputImg = styled.input`
+  position: absolute;
+  opacity: ${({ selectedImage }) => (selectedImage ? 0 : 1)};
+  width: 131px;
+  height: 131px;
+`;
+
+const PreviewImage = styled.img`
+  width: 131px;
+  height: 131px;
+  border-radius: 5px;
+  margin-top: 10px;
+  object-fit: cover;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1;
+`;
+
+const ConditionP = styled.div`
+  font-family: "Pretendard-Regular";
+  color: rgba(255, 255, 255, 0.6);
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+
+  margin-top: 7px;
 `;
 
 const Btn = styled.button`
@@ -386,4 +522,18 @@ const Btn = styled.button`
   font-size: 18px;
   font-style: normal;
   font-weight: 600;
+
+  ${(props) =>
+    !props.disabled &&
+    css`
+      background: var(--main-purple, #5a45f5);
+      color: #fff;
+      cursor: pointer;
+    `}
+`;
+
+const InputI = styled(InputGuide)`
+  width: 350px;
+  height: 46px;
+  color: var(--sub-purple, #a397ff);
 `;
